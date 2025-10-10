ce923159ec8f8e072b4a2f71e15a48208db3d44a
David Němec <nemecd8128@gmail.com>
2025-10-10 11:12:47 +0200

    test load table

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 x = np.linspace(0, 5, 100)
 y = x**3
@@ -16,7 +17,13 @@ plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.title('Scatter plot of y = x^3')
 
-plt.show()
+#plt.show()
+plt.close()
+
+df = pd.read_excel('uloha26/uloha_26.xlsx', sheet_name='List2', skiprows=1, usecols='D:M', nrows=6)
+print(df)
+print(df.to_latex(index=False))
+
 
 
 
