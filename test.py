db48d61a24150354d24a41b637ad552b79a259ce
David Němec <nemecd8128@gmail.com>
2025-10-12 20:10:41 +0200

    t

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from uncertainties import ufloat, unumpy as unp
 
 x = np.linspace(0, 5, 100)
 y = x**3
@@ -24,7 +25,8 @@ df = pd.read_excel('uloha26/uloha_26.xlsx', sheet_name='List2', skiprows=1, usec
 print(df)
 print(df.to_latex(index=False))
 
-
+x = unp.uarray([1.34, 134.5544, 24.4455], [0.001, 0.0785, 0.02])
+print(x)
 
 
 print('All done!')
