ac165b42a2f193559f692ad83530334926ccf78e
David Němec <nemecd8128@gmail.com>
2025-11-05 16:34:43 +0100

    uloha 9 init

---
+++ code9.py
@@ -0,0 +1,10 @@
+import numpy as np
+from numpy.polynomial.polynomial import Polynomial
+import pandas as pd
+from uncertainties import ufloat, unumpy as unp, umath as um
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import praktika as pr
+from praktika import Var
+from matplotlib.lines import Line2D
+from scipy.interpolate import make_interp_spline
